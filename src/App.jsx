fb52435109cf6c39d6868a3b82da588f407c877e
Hans Ellis <bullhorn@gmail.com>
2023-05-08 18:48:01 -0700

    finish setup in src files, set navbar and finalize gradient

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Navbar } from "./components";
+import styles from "./style"
+
+const App = () => (
+    <div className="bg-primay w-full overflow-hidden">
+      <div className={`bg-blue-gradient ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          The Aprons
+          <br />
+          About
+          <br />
+          What users are saying.
+          <br />
+          Locations
+          <br />
+          Contact
+          <br />
+          Make an apron!
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home"
+  },
+  {
+    id: "aprons",
+    title: "Aprons"
+  },
+  {
+    id: "testimonials",
+    title: "Testimonials"
+  },
+  {
+    id: "contact",
+    title: "Contact"
+  }
+]
