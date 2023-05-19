ee7a644702f81ed3dddf37a1411cd3456261c9b3
Hans Ellis <bullhorn@gmail.com>
2023-05-19 00:36:50 -0700

    tweak text styling on hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,15 @@ const Hero = () => (
       <div className="flex flex-col py-[6px] px-4 mb-2">
         <img src={schurzscript} alt="schurzenliebe banner" />
         <div className="text-center">
-          <h1 className="text-3xl font-homemade leading-2 text-gray-600">Handmade Crossback Aprons</h1>
+          <h1 className=" md:text-2xl xl:text-3xl text-lg font-homemade leading-2 text-gray-600">Handmade Crossback Aprons</h1>
         </div >
-        <div className={`flex justify-center items-center sm:flex-row flex-col mx-auto py-6`}>
+        <div className={`flex justify-center items-center sm:flex-row flex-col mx-auto py-16`}>
           <img 
           src={schurzhero} 
           alt="woman in kitchen wearing schurzenlibe apron"
           className="w-[500px]"
           />
-          <h1 className="flex font-homemade font-semibold self-end text-greenText text-right ss:text-[75px] text-[52px] ss:leading-[85px] leading-[75px]">
+          <h1 className="flex font-homemade font-semibold self-end text-greenText text-right ss:text-[75px] text-[52px] ss:leading-[85px] leading-[75px] py-6">
             for the love of aprons
           </h1>
         </div> 
